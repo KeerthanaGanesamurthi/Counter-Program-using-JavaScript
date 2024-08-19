6ac411ac675b0b7c5d12341ca024fd464ea29079
Keerthana <126674855+KeerthanaGanesamurthi@users.noreply.github.com>
2024-08-19 14:29:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+//COUNTER PROGRAM
+
+const decreaseBtn = document.getElementById("decreaseBtn");
+const resetBtn = document.getElementById("resetBtn");
+const increaseBtn = document.getElementById("increaseBtn");
+const countlabel =  document.getElementById("countlabel");
+let count = 0;
+
+resetBtn.onclick = function(){
+    count = 0;
+    countlabel.textContent = count;
+}
+
+increaseBtn.onclick = function(){
+    count++;
+    countlabel.textContent = count;
+}
+
+decreaseBtn.onclick = function(){
+    count--;
+    countlabel.textContent = count;
+}
+
+
+
